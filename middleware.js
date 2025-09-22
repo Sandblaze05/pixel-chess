19736a01097452058b0462d81353e614dfd3d883
Sandblaze05 <tejaschauhan0512@gmail.com>
2025-09-22 21:51:30 +0530

    fix

--- middleware.js
+++ middleware.js
@@ -1,3 +1,9 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ['/dashboard/:path*', '/profile/:path*'] };
+export const config = { 
+  matcher: [
+    '/dashboard/:path*', 
+    '/profile/:path*'
+    // Only list the specific routes you want to protect
+  ] 
+}
