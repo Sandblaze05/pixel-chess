2187fff55e87e941d882718ffe3ae048af65070b
Sandblaze05 <tejaschauhan0512@gmail.com>
2025-09-22 22:19:37 +0530

    switching to vanilla redirect

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,7 +1,16 @@
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
+
 export const metadata = {
     title: 'Dashboard'
 }
 
-export default function DashboardLayout({ children }) {
+export default async function DashboardLayout({ children }) {
+    const session = await getServerSession();
+
+    if (!session) {
+        redirect('/login');
+    }
+
     return <div className="min-h-screen flex flex-col">{children}</div>;
 }
--- middleware.js
+++
@@ -1,9 +0,0 @@
-export { default } from "next-auth/middleware"
-
-export const config = { 
-  matcher: [
-    '/dashboard/:path*', 
-    '/profile/:path*'
-    // Only list the specific routes you want to protect
-  ] 
-}
